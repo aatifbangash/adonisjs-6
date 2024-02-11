04a628891575f9191b728c7a1010152e8cd5cf95
atif <atif.ali@nmxglobalsoftware.com>
2024-02-11 09:33:38 +0500

    c

--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,7 @@ import router from '@adonisjs/core/services/router'
 const HomeController = () => import('#controllers/home_controller')
 
 router.get('/', [HomeController, 'index'])
+router.on('/single').render('pages/single')
 router.get('/create', [HomeController, 'create'])
 router.get('/search', [HomeController, 'search'])
 
